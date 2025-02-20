924095a4e65c4f5688d6456daffdfafd42e2d9a0
DarshiniVythilingum <pillayvythilingumdarshini@gmail.com>
2025-02-19 23:22:42 -0500

    upgrade docker image

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Week 1 : Trends in Tech class
-# Topic of the Day " How to work with GIT
+#
+
+docker run -p 8081:80 -v C:\Projects\<YOUR PROJECT FOLDER>\dist:/usr/share/nginx/html -d nginx
+
+#
+
+docker build -t darsh/tit:v1 .
+
+#
+
+docker run -e MARIADB_ROOT_PASSWORD=123456 -e MARIADB_DATABASE-in-class-db -e MARIADBL_USER-in-class-user -e MARIADB_PASSWORD=123456 -p 3306:3306 -d mariadb
+I
--- api/index.js
+++ api/index.js
@@ -3,15 +3,18 @@ const express = require("express");
 const mysql = require("mysql2/promise"); // Using mysql2 with Promises
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+
+const dotenv = require('dotenv');
+dotenv.config({path: path.join(_dirname, ".env")});
 
 // Database Configuration
 const dbConfig = {
-    host: "localhost", 
+    host: "127.0.0.1", 
     port: 3306, //docker ps shows port 3306 for mariadb
-    user: "root",
-    password: "123456",
-    database: "in-class-db"
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PASSWORD,
+    database: process.env.MARIADB_DATABASE,
 };
 
 app.use(express.json());
@@ -40,12 +43,13 @@ app.post("/api/message", async (req, res) => {
     }
 });
 
+//Main route of API
 app.get("/api/messages", async (req, res) => {
     try {
         const conn = await mysql.createConnection(dbConfig);
         const [rows] = await conn.execute("SELECT * FROM users"); 
         await conn.end();
-        res.status(200).json(rows); //array of message objects
+        res.status(200).json({error: "All fields are required!"}); //array of message objects
     } catch (error) {
         res.status(500).json({ error: "Fail" }); 
     }
@@ -54,40 +58,40 @@ app.get("/api/messages", async (req, res) => {
 
 //PART 2
 // Initialize the database
-async function initDatabase() {
-    try {
-        const conn = await mysql.createConnection(dbConfig);
+// async function initDatabase() {
+//     try {
+//         const conn = await mysql.createConnection(dbConfig);
 
-        // Check if the table exists
-        const [tables] = await conn.query("SHOW TABLES LIKE 'messages'");
+//         // Check if the table exists
+//         const [tables] = await conn.query("SHOW TABLES LIKE 'messages'");
 
-        if (tables.length === 0) {
-            const createTableQuery = `
-                CREATE TABLE messages (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(200) NOT NULL,
-                    email VARCHAR(200) NOT NULL,
-                    message TEXT NOT NULL,
-                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                )
-            `;
+//         if (tables.length === 0) {
+//             const createTableQuery = `
+//                 CREATE TABLE messages (
+//                     id INT AUTO_INCREMENT PRIMARY KEY,
+//                     name VARCHAR(200) NOT NULL,
+//                     email VARCHAR(200) NOT NULL,
+//                     message TEXT NOT NULL,
+//                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//                 )
+//             `;
 
-            await conn.query(createTableQuery);
-            console.log("Table 'messages' created successfully.");
-        } else {
-            console.log("Table 'messages' already exists.");
-        }
+//             await conn.query(createTableQuery);
+//             console.log("Table 'messages' created successfully.");
+//         } else {
+//             console.log("Table 'messages' already exists.");
+//         }
 
-        await conn.end(); // Close connection after execution
-    } catch (error) {
-        console.error("Database ERROR:", error);
-        process.exit(1);
-    }
-}
+//         await conn.end(); // Close connection after execution
+//     } catch (error) {
+//         console.error("Database ERROR:", error);
+//         process.exit(1);
+//     }
+// }
 
 // Call the function to initialize the database
-initDatabase().then(() => {
-    app.listen(port, () => {
-        console.log(`The server is running, PORT: ${port}`);
+// initDatabase().then(() => {
+    app.listen(process.env.API_PORT, () => {
+        console.log(`The server is running, PORT: ${process.env.API_PORT}`);
     });
-});
+// });
